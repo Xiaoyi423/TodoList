ae0b31eeb048c6c2a82c846057039b72802ff909
Xiaoyi423 <96610420+Xiaoyi423@users.noreply.github.com>
2022-08-04 00:19:39 -0400

    add work list page

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let items = [];
+const items = [];
+const workItems = [];
 //setting view engine to ejs
 app.set("view engine", "ejs");
 app.use(express.static("public"))
@@ -19,15 +20,23 @@ app.get("/", function (req, res) {
     month: 'long'
   };
   let day = today.toLocaleDateString("en-US", options)
-  res.render("list", { kindOfDay: day, newListItems: items })
+  res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.listen(3000, function () {
   console.log("server started on port 3000");
 });
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
 
 app.post("/", function (req, res) {
   let item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 });
